d9ec8079d35ac769c34023b6cdb025bb25203e04
P3ND4 <106392394+P3ND4@users.noreply.github.com>
2025-07-31 17:01:00 -0800

    making navigation bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: App,
+        children: [
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+            { path: 'dashboard', component: Dashboard },
+            { path: 'login', redirectTo: 'dashboard', pathMatch: 'full' },
+        ]
+    }
+];
