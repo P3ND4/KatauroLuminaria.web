487c4cf72f1772cb9e8be4b5b7175cff3d084329
P3ND4 <106392394+P3ND4@users.noreply.github.com>
2025-08-05 07:37:56 -0800

    static views of nav var and footer almost done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { App } from './app';
-import { Dashboard } from './dashboard/dashboard';
+import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
     {
