22f4127c5a47f84a4794c46e10977cbfcfc7a0c8
P3ND4 <106392394+P3ND4@users.noreply.github.com>
2025-09-12 08:52:17 -0500

    car service

--- src/app/shared/services/cart/cart.service.ts
+++ src/app/shared/services/cart/cart.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../../models/Products';
+import { HttpService } from '../http/http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   currentProducts: Product[] = []
+
+  constructor(private http: HttpService){
+    http.getProducts().subscribe({
+      next: (val)=>{ this.currentProducts = val as Product[]; },
+      error: (err) => {console.log(err); }
+    });
+  }
 
   addToCart(product: Product){
     this.currentProducts.push(product);
